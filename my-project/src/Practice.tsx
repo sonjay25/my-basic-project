b6d65d29074e423aebbb14863c15f1bd7a82a9e8
jayson <jayson.song25@gmail.com>
2024-10-02 09:04:09 +0800

    pogi ko po promises

--- my-project/src/Practice.tsx
+++ my-project/src/Practice.tsx
@@ -1,15 +1,45 @@
-type P = (x: number) => number;
-type Q = {fname: string, age: number, item?: number[]};
+
+interface Person<T,U, V> {
+  fname: T
+  age: U
+  add?: V;
+  function?: () => string;
+};
+type Arr<T> = T[];
+type Str<T, U> = {fname: T, age: U} | {add: T, user: U};
+
+
 function Practice() {
-  function myf(a: number[]) {
-    return a.filter(fil => fil >= 5).map(value => value * 2);
+ 
+
+
+
+ const dre = async () => {
+    try {
+      let res = await fetch('gwapo/ko');
+    let result = await res.json();
+    console.log(result)
+    } catch (error) {
+      console.log(error)
+    }
   }
-  const t = [1, 3, 5, 5, 68];
-  console.log(myf(t))
+  setTimeout(() => {
+    dre()
+  }, 4000);
+  
+
+
+
+
+
+  
+  
+ 
+
   return (
-    <div>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio earum ut facilis necessitatibus laborum, recusandae repellendus ab. Error, numquam aliquid.</p>
-   </div>
+    <>
+
+    </>
   )
 }
 
